8340875358733cd37a4f38ca259cf9cf91459f0d
Israel Zablianov <123hartk123@gmail.com>
2018-05-26 18:09:52 +0300

    changing hello component

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -1,64 +1,82 @@
-// src/components/Hello.tsx
-import * as React from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import * as React from "react"
+import { Button, StyleSheet, Text, View } from "react-native"
 
 export interface Props {
-  name: string;
-  enthusiasmLevel?: number;
-  onIncrement?: () => void;
-  onDecrement?: () => void;
+  name: string
+  enthusiasmLevel?: number
 }
 
-function Hello({ name, enthusiasmLevel = 1, onIncrement, onDecrement }: Props) {
-  if (enthusiasmLevel <= 0) {
-    throw new Error('You could be a little more enthusiastic. :D');
+interface State {
+  enthusiasmLevel: number
+}
+
+export class Hello extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props)
+
+    if ((props.enthusiasmLevel || 0) <= 0) {
+      throw new Error("You could be a little more enthusiastic. :D")
+    }
+
+    this.state = {
+      enthusiasmLevel: props.enthusiasmLevel || 1
+    }
   }
 
-  return (
-    <View style={styles.root}>
-        <Text style={styles.greeting}>
-            Hello {name + getExclamationMarks(enthusiasmLevel)}
-        </Text>
-        <View style={styles.buttons}>
-            <View style={styles.button}>
-                <Button title="-" onPress={onDecrement || (() => {})} color='red' />
-            </View>
-            <View style={styles.button}>
-                <Button title="+" onPress={onIncrement || (() => {})} color='blue' />
-            </View>
-        </View>
-    </View>
-  );
-}
+  onIncrement = () => this.setState({ enthusiasmLevel: this.state.enthusiasmLevel + 1 });
+  onDecrement = () => this.setState({ enthusiasmLevel: this.state.enthusiasmLevel - 1 });
+  getExclamationMarks = (numChars: number) => Array(numChars + 1).join("!")
 
-export default Hello;
+  render() {
+    return (
+      <View style={styles.root}>
+        <Text style={styles.greeting}>
+          Hello {this.props.name + this.getExclamationMarks(this.state.enthusiasmLevel)}
+        </Text>
+        
+        <View style={styles.buttons}>
+          <View style={styles.button}>
+            <Button
+              title="-"
+              onPress={this.onDecrement}
+              accessibilityLabel="decrement"
+              color="red"
+            />
+          </View>
+
+          <View style={styles.button}>
+            <Button
+              title="+"
+              onPress={this.onIncrement}
+              accessibilityLabel="increment"
+              color="blue"
+            />
+          </View>
+        </View>
+      </View>
+    )
+  }
+}
 
 // styles
-
 const styles = StyleSheet.create({
-    root: {
-        alignItems: "center",
-        alignSelf: "center"
-    },
-    buttons: {
-        flexDirection: "row",
-        minHeight: 70,
-        alignItems: "stretch",
-        alignSelf: "center",
-        borderWidth: 5,
-    },
-    button: {
-        flex: 1,
-        paddingVertical: 0,
-    },
-    greeting: {
-        color: "#999",
-        fontWeight: "bold"
-    }
-});
-
-// helpers
-
-function getExclamationMarks(numChars: number) {
-  return Array(numChars + 1).join('!');
-}
+  root: {
+    alignItems: "center",
+    alignSelf: "center"
+  },
+  buttons: {
+    flexDirection: "row",
+    minHeight: 70,
+    alignItems: "stretch",
+    alignSelf: "center",
+    borderWidth: 5
+  },
+  button: {
+    flex: 1,
+    paddingVertical: 0
+  },
+  greeting: {
+    color: "#999",
+    fontWeight: "bold"
+  }
+})
