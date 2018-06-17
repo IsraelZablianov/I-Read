7bac2b820c064e29854ef7d205b3f2a68b0daf55
Israel Zablianov <123hartk123@gmail.com>
2018-06-17 22:01:34 +0300

    fixing ios files

--- index.ios.js
+++ index.ios.js
@@ -1,2 +1,8 @@
+import * as React from 'react';
+import IRead from "./lib/index.ios";
 
-import './index.ios';
+import {
+    AppRegistry
+} from 'react-native';
+
+AppRegistry.registerComponent('IRead', () => IRead);
